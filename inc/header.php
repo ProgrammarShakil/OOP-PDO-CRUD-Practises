7f58cdb30112c5065d17d48c5eb7c6bc8bc601c9
shakil islam <freelancershakil913@gmail.com>
2021-10-02 15:56:42 +0600

    basic crud preparetion

---
+++ inc/header.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FS Ecom</title>
+
+    <!-- Bootstrap CDN  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
+    <!-- OWL Carousel CDN  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- Font awesome Icons  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- Custom css file  -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <!-- start header  -->
+    <header id="header">
+        <div class="text-center py-2 bg-light">
+            <div class="font-rale display-4 text-black-50">Student List</div>
+        </div>
+    </header>
+    <!-- !start header  -->
--- index.php
+++ index.php
@@ -7,8 +7,67 @@ try {
   $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
+  echo "";
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
 ?>
+<?php include "inc/header.php"; ?>
+
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+          <!-- start main  -->
+    <main>
+     <h5 class="text-end"><a style="color:#fff; border-radius:5px"  class="text-decoration-none bg-secondary pb-1 px-1" href=""><i class="fa fa-plus" aria-hidden="true"></i> Add Student</a></h5>
+     <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Serial</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>01</td>
+                        <td>Freelancer Shakil</td>
+                        <td>Freelancershakil913@gmail.com</td>
+                        <td>01770345676</td>
+                        <td>
+                          <a style="color:aliceblue; border-radius:5px" class=" p-1 text-decoration-none bg-secondary  me-2" class="" href=""> <i class="fas fa-edit" aria-hidden="true"></i>  Edit</a> 
+                          <a onclick="return confirm('Are you sure ?')" style="color:aliceblue;  border-radius:5px" class="p-1 text-decoration-none bg-secondary" href=""> <i class="fas fa-trash" aria-hidden="true"></i> Delete</a>
+                        </td>
+                    </tr>
+                    <tr>
+                    <td>01</td>
+                        <td>Freelancer Shakil</td>
+                        <td>Freelancershakil913@gmail.com</td>
+                        <td>01770345676</td>
+                        <td>
+                          <a style="color:aliceblue; border-radius:5px" class=" p-1 text-decoration-none bg-secondary me-2" class="" href=""><i class="fas fa-edit" aria-hidden="true"></i> Edit</a> 
+                          <a onclick="return confirm('Are you sure ?')" style="color:aliceblue;  border-radius:5px" class="p-1 text-decoration-none bg-secondary" href=""> <i class="fas fa-trash" aria-hidden="true"></i> Delete</a>
+                        </td>
+                    </tr>
+                    <tr>
+                    <td>01</td>
+                        <td>Freelancer Shakil</td>
+                        <td>Freelancershakil913@gmail.com</td>
+                        <td>01770345676</td>
+                        <td>
+                          <a style="color:aliceblue; border-radius:5px" class=" p-1 text-decoration-none bg-secondary  me-2" class="" href=""> <i class="fas fa-edit" aria-hidden="true"></i> Edit</a> 
+                          <a onclick="return confirm('Are you sure ?')" style="color:aliceblue;  border-radius:5px" class="p-1 text-decoration-none bg-secondary" href=""> <i class="fas fa-trash" aria-hidden="true"></i> Delete</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+    </main>
+    <!-- !start main  -->
+    </div>
+  </div>
+</div>
+
+    <?php include "inc/footer.php"; ?>
